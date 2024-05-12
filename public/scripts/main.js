6aa05999f6e9d883c961d22f439b06a04473654c
aplaza2 <aplaza2@uc.cl>
2024-05-12 18:12:56 -0400

    Boton broma agregado

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -38,14 +38,52 @@ function toggleOnlineOffline() {
 // Funcion para mostrar notificaciones
 function notificationButton() {
     const notificationButton = document.getElementById('notificationButton');
+    const notificationIcon = document.getElementById('notificationIcon');
+
     notificationButton.addEventListener('click', () => {
+        if (Notification.permission === 'granted') {
+            new Notification('Las notificaciones ya están activadas');
+            notificationButton.classList.remove('btn-ghost');
+            notificationIcon.classList.remove('invisible');
+                setTimeout(() => {
+                    notificationIcon.classList.add('invisible');
+                }, 3000);
+            return;
+        }
         Notification.requestPermission().then((result) => {
             if (result === 'granted') {
                 new Notification('Se han activado las notificaciones');
                 notificationButton.classList.remove('btn-ghost');
-            }
+                notificationIcon.classList.remove('invisible');
+                setTimeout(() => {
+                    notificationIcon.classList.add('invisible');
+                }, 3000);
+                }
         });
     });
+}
+
+function surpriseButton() {
+    const surpriseButton = document.getElementById('surprise');
+    surpriseButton.addEventListener('click', () => {
+        if (Notification.permission != 'granted') {
+            Notification.requestPermission().then((result) => {
+                if (result != 'granted') {
+                    return;
+                }
+            });
+        }
+        navigator.geolocation.getCurrentPosition(showPosition);
+    });
+}
+
+function showPosition(position) {
+    new Notification('Ubicación', { body: 'Te encontramos' });
+    setTimeout(() => {
+        let msg = "Latitude: " + position.coords.latitude;
+        msg += "\nLongitude: " + position.coords.longitude;
+        new Notification('Acabas de ser doxxeado', { body: msg });
+    }, 10000);
 }
 
 // Funcion para renderizar componentes que se repiten en diferntes vistas (navbar y footer)
@@ -60,6 +98,7 @@ async function loadComponent(component, id) {
         if (component === 'navbar') {
             toggleOnlineOffline();
             notificationButton();
+            surpriseButton();
         }
     });
 }
@@ -83,3 +122,10 @@ document.addEventListener('DOMContentLoaded', function() {
         notePadName.value = '';
     });
 })
+
+// Funcion para mostrar notificaciones
+window.addEventListener('offline', () => {
+    if (Notification.permission === 'granted') {
+        new Notification('Se ha perdido la conexión a internet');
+    }
+});
